0a8da408dd075bba207135fdb4e22d8211451e32
Oliver Dixon <od641@york.ac.uk>
2026-04-30 14:03:52 +0100

    Added backtracking/searching to SW prototype.

---
+++ sw-prototyping/src/search_driver.cpp
@@ -0,0 +1,195 @@
+/**
+ * @file
+ * @brief Backtracking and search implementation, intended to be executed by the ARM Cortex.
+ */
+
+#include <cassert>
+#include <cstring>
+#include <iostream>
+
+#include "search_driver.hpp"
+
+#define MAX_SEARCH_DEPTH (512)
+
+namespace {
+
+struct CellRef
+{
+    extent_t row;
+    extent_t col;
+    bool valid;
+};
+
+/**
+ * @brief Produce a reference to the first encountered cell without an assignment.
+ * @param black Black cell assignments.
+ * @param white White cell assignments.
+ * @param puzzle_extent Extent of the square puzzle grid.
+ * @return A CellRef containing indices of the first-encountered unassigned cell; if the
+ *  <code>valid</code> flag is not set, the indices are garbage.
+ * @post If the returned CellRef is valid, the row and column indices refer to a puzzle square that
+ *  is not assigned.
+ * @todo This is quite a simple heuristic used by the branch/search algorithm.
+ */
+CellRef choose_unknown(
+    const line_t * const black,
+    const line_t * const white,
+    const extent_t puzzle_extent
+) {
+    CellRef choice = {.valid = false};
+
+    for (extent_t row_idx = 0; row_idx < puzzle_extent; ++row_idx) {
+        const line_t known = black[row_idx] | white[row_idx];
+        for (extent_t col_idx = 0; col_idx < puzzle_extent; ++col_idx) {
+            const line_t col_mask = 1U << col_idx;
+            if ((known & col_mask) == 0) {
+                choice.valid = true;
+                choice.row = row_idx;
+                choice.col = col_idx;
+
+                assert((known & col_mask) == 0);
+                return choice;
+            }
+        }
+    }
+
+    return choice;
+}
+
+/**
+ * @brief Helper to recurse down a single branch by preparing and maintaining the propagation lines.
+ * @param row_patterns Precomputed valid row patterns.
+ * @param row_counts Indexed map for sizes of each row pattern.
+ * @param col_patterns Precomputed valid column patterns.
+ * @param col_counts Indexed map for sizes of each column pattern.
+ * @param puzzle_size The extent of the square puzzle.
+ * @param in_black Input black cell assignments.
+ * @param in_white Input white cell assignments.
+ * @param out_black Output black cell assignments, updated iff the search produced a valid solution.
+ * @param out_white Output white cell assignments, updated iff the search produced a valid solution.
+ * @param depth Recursion depth: initialise to zero.
+ * @return Result of the search down the set branch.
+ */
+SearchResult search_branch(
+    const line_t * const row_patterns,
+    const extent_t * const row_counts,
+    const line_t * const col_patterns,
+    const extent_t * const col_counts,
+    const extent_t puzzle_size,
+    const line_t * const in_black,
+    const line_t * const in_white,
+    line_t * const out_black,
+    line_t * const out_white,
+    const unsigned int depth
+) {
+    line_t propagated_black[MAX_SIZE];
+    line_t propagated_white[MAX_SIZE];
+
+    std::memcpy(propagated_black, in_black, puzzle_size * sizeof(line_t));
+    std::memcpy(propagated_white, in_white, puzzle_size * sizeof(line_t));
+
+    const SearchResult result = search(
+        row_patterns, row_counts, col_patterns, col_counts, puzzle_size, propagated_black,
+        propagated_white, depth + 1
+    );
+
+    if (result == SEARCH_SOLVED) {
+        std::memcpy(out_black, propagated_black, puzzle_size * sizeof(line_t));
+        std::memcpy(out_white, propagated_white, puzzle_size * sizeof(line_t));
+        return SEARCH_SOLVED;
+    }
+
+    return result;
+}
+
+} // namespace
+
+SearchResult search(
+    const line_t * const row_patterns,
+    const extent_t * const row_counts,
+    const line_t * const col_patterns,
+    const extent_t * const col_counts,
+    const extent_t puzzle_size,
+    line_t * const black,
+    line_t * const white,
+    const unsigned int depth
+) {
+    if (depth > MAX_SEARCH_DEPTH)
+        return SEARCH_UNKNOWN;
+
+    line_t propagated_black[MAX_SIZE];
+    line_t propagated_white[MAX_SIZE];
+
+    // Do an initial solve attempt with the input grid assignments to see if we have a trivial case.
+
+    const SolverState status = solve(
+        row_patterns, row_counts, col_patterns, col_counts, puzzle_size, black, white,
+        propagated_black, propagated_white
+    );
+
+    if (status == SOLVER_CONTRADICTION)
+        return SEARCH_FAILED;
+
+    if (status == SOLVER_OK) {
+        std::memcpy(black, propagated_black, puzzle_size * sizeof(line_t));
+        std::memcpy(white, propagated_white, puzzle_size * sizeof(line_t));
+        return SEARCH_SOLVED;
+    }
+
+    /*
+     * If there's no trivial solution, do a search. Use a simple heuristic (first observed cell
+     * without an assignment in either black or white) and pivot there: first assume black; reset;
+     * then assume white; reset.
+     */
+
+    const CellRef choice = choose_unknown(propagated_black, propagated_white, puzzle_size);
+    const line_t col_mask = 1U << choice.col;
+
+    if (!choice.valid)
+        /*
+         * If an unknown cell couldn't be chosen, all cells must have assignments. In this case, one
+         * assignment must be wrong, so backtrack.
+         */
+        return SEARCH_UNKNOWN;
+
+    // Assume the unknown cell is black and recurse.
+
+    assert((propagated_black[choice.row] & col_mask) == 0);
+    propagated_black[choice.row] |= col_mask;
+
+    const SearchResult first_result = search_branch(
+        row_patterns, row_counts, col_patterns, col_counts, puzzle_size, propagated_black,
+        propagated_white, black, white, depth
+    );
+
+    propagated_black[choice.row] &= ~col_mask;
+
+    if (first_result == SEARCH_SOLVED)
+        return SEARCH_SOLVED;
+
+    // If the black assumption didn't yield anything, assume the unknown cell is white and recurse.
+
+    assert((propagated_white[choice.row] & col_mask) == 0);
+    propagated_white[choice.row] |= col_mask;
+
+    const SearchResult second_result = search_branch(
+        row_patterns, row_counts, col_patterns, col_counts, puzzle_size, propagated_black,
+        propagated_white, black, white, depth
+    );
+
+    propagated_white[choice.row] &= ~col_mask;
+
+    if (second_result == SEARCH_SOLVED)
+        return SEARCH_SOLVED;
+
+    /*
+     * If neither search yielded something useful, the grid is either definitively unsolvable (if
+     * the solver returned a contradiction), or the result is known (happens in case of exceeding
+     * stack depth limits).
+     */
+
+    if (first_result == SEARCH_UNKNOWN || second_result == SEARCH_UNKNOWN)
+        return SEARCH_UNKNOWN;
+
+    return SEARCH_FAILED;
+}
---
+++ sw-prototyping/src/search_driver.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @brief Backtracking and search interface, intended to be executed by the ARM Cortex.
+ */
+
+#ifndef SW_PROTOTYPING_SEARCH_DRIVER_HPP
+#define SW_PROTOTYPING_SEARCH_DRIVER_HPP
+
+#include "solver.h"
+
+enum SearchResult
+{
+    SEARCH_SOLVED,
+    SEARCH_FAILED,
+    SEARCH_UNKNOWN
+};
+
+/**
+ * @brief Attempt to solve the Nonogram described by the given clues using backtracking and search.
+ * @param row_patterns Precomputed valid row patterns.
+ * @param row_counts Indexed map for sizes of each row pattern.
+ * @param col_patterns Precomputed valid column patterns.
+ * @param col_counts Indexed map for sizes of each column pattern.
+ * @param puzzle_size The extent of the square puzzle.
+ * @param black Input/output black cell assignments.
+ * @param white Input/output white cell assignments.
+ * @param depth Recursive call depth: initialise to zero.
+ * @return The result of the search: either the Nonogram was solved (satisfactory assignments),
+ *  failed (provable contradiction), or was unknown (due to timeout).
+ */
+SearchResult search(
+    const line_t * row_patterns,
+    const extent_t * row_counts,
+    const line_t * col_patterns,
+    const extent_t * col_counts,
+    extent_t puzzle_size,
+    line_t * black,
+    line_t * white,
+    unsigned int depth
+);
+
+#endif // SW_PROTOTYPING_SEARCH_DRIVER_HPP
